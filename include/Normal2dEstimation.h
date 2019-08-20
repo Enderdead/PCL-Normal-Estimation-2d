7f74d3f7f4a441f426b46b45c8f62638ce5bbfe8
François GAUTHIER-CLERC <francois.gauthier-clerc@basystemes.fr>
2019-08-20 11:00:56 +0200

    Add doc string.

--- include/Normal2dEstimation.h
+++ include/Normal2dEstimation.h
@@ -26,68 +26,189 @@ typedef pcl::search::Search<Point>::Ptr PtrkdTree;
 typedef boost::shared_ptr<std::vector<int> > IndicesPtr;
 
 
+
+/** \brief Normal2dEstimation estimate local straight line at each points using it neighbour. Contrary to native normal
+  * estimation  on PCL lib, it works only on 2D points cloud.
+  *
+  * \note This class use 3D points cloud but ignore all z components. So make sure you have all your points on z=0 plan.
+  *
+  *
+  */
 class Normal2dEstimation {
 public:
 
+    /**
+     * \brief Classic constructor, you can directly setup parameters using constructor arguments.
+     *
+     * @param search_radius If different from zero, the normal estimation will use the radius to find out which points
+     *                      it take for line estimation.
+     *
+     * @param k If different from zero, the normal estimation will find the kth closest points for computing line estimation.
+     */
     Normal2dEstimation(double search_radius=0.0, unsigned int k=0) : m_search_radius(search_radius), m_k(k), m_indices(new std::vector<int>), m_kd_tree(nullptr), m_in_cloud(nullptr), m_view_point(0,0,0) {}
 
 
+    /**
+     * \brief Compute on each point the normal estimation.
+     * @param normal_cloud Normal cloud to fill with normal estimation result.
+     */
     void compute(const PtrCloud& normal_cloud) const;
 
+
+    /**
+     * \brief Compute on each point the normal estimation.
+     * @param normal_cloud Normal cloud to fill with normal estimation result.
+     */
     void compute(const pcl::PointCloud<pcl::Normal>::Ptr& normal_cloud) const;
 
+    /**
+     *  \brief Setup the view point
+     *
+     *  The view points is used in normal orientation purpose. This orientation force all normals to look at view points.
+     *
+     * @param origin View point position.
+     */
     void setViewPoint(const Point& origin)  {m_view_point = origin;}
 
+    /**
+     * \brief return the current view point.
+     */
     Point getViewPoint() const {return m_view_point;}
 
+    /**
+     *  \brief Setup a search radius for normal estimation on each point.
+     * @param search_radius Max distance for search radius (cartesian distance).
+     */
     void setRadiusSearch(double search_radius) {m_search_radius = search_radius;}
 
+    /**
+     * \brief Prove current radius for normal estimation.
+     * @return Current search radius.
+     */
+    inline double getRadiusSearch() const {return m_search_radius;}
+
+    /** \brief Provide a pointer to the search object.
+     * \param kdtree tree a pointer to the spatial search object.
+     */
     void setSearchMethod(const PtrkdTree& kdtree) {m_kd_tree = kdtree;}
 
+
+    /**
+     * \brief Setup the cluster size for normal estimation.
+     * @param k cluster size for normal estimation.
+     */
     void setKSearch(unsigned int k) {m_k =k;}
 
-    int getKSearch() const {return m_k;}
+    /**
+     * \brief Provide the current k constant.
+     * @return k constant.
+     */
+    inline int getKSearch() const {return m_k;}
 
+    /**
+     *  \brief Provide a input  cloud pointer to deal with.
+     *  @param cloud Cloud to deal with.
+     */
     void setInputCloud(const ConstPtrCloud& cloud);
 
-    ConstPtrCloud getInputCloud() const {return m_in_cloud;}
+    /**
+     * \brief Provide the current cloud set as input.
+     * @return A pointer to current input cloud.
+     */
+    inline ConstPtrCloud getInputCloud() const {return m_in_cloud;}
 
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+      * \param indices indices a pointer to the indices that represent the input data.
+      */
     void setIndices(const pcl::PointIndices::Ptr& indices);
 
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+      * \param indices indices a pointer to the indices that represent the input data.
+      */
     void setIndices(const pcl::PointIndices::ConstPtr& indices);
 
 
 private:
 
+    /** \brief Search for nearest neighbors using the spatial locator from search class and class parameters (k and search_radius).
+      *
+      * \param index index the index of the query point.
+      * \param[out] nn_indices the resultant vector of indices representing the neighbors cluster.
+      * \param[out] nn_dists the resultant vector of distances representing the distances from the query point to the neighbors.
+      *
+      * \return The number of neighbors found. If no neighbors are found or an error occurred, return 0.
+      */
     int searchForNeighbors(int index, std::vector<int>& nn_indices,std::vector<float>& nn_dists) const;
 
 
+    /** \brief Compute the Least-Squares straight line fit for a given set of points, using their indices,
+      * and return the estimated normal parameters.
+
+      * \param indices the point cloud indices that need to be used.
+        * \param nx the resultant X component of the plane normal.
+        * \param ny the resultant Y component of the plane normal.
+        * \param nz the resultant Z component of the plane normal (always 0).
+      * @return Return true if a normal can be computed, false 	otherwise.
+      */
     bool computePointNormal2d (boost::shared_ptr<std::vector<int>>& indices,
                                float &nx, float &ny, float &nz) const;
 
+    /** \brief Compute the Least-Squares straight line fit for a given set of points, using their indices,
+      * and return the estimated normal parameters.
+      *
+      * \param indices the point cloud indices that need to be used.
+      * \param nx the resultant X component of the plane normal.
+      * \param ny the resultant Y component of the plane normal.
+      * \param nz the resultant Z component of the plane normal (always 0).
+      * \param curvature the estimated surface curvature as a measure of
+      * \f[
+      * \lambda_0 / (\lambda_0 + \lambda_1)
+      * \f]
+      *
+      * @return Return true if a normal can be computed, false otherwise.
+      */
     bool computePointNormal2d (boost::shared_ptr<std::vector<int>>& indices,
                                float &nx, float &ny, float &nz, float& curvature) const;
-
+    /** \brief Compute the Least-Squares straight line fit for a given set of points, using their indices,
+      * and return the estimated normal parameters.
+      * \param indices the point cloud indices that need to be used.
+      * \param normal parameter.
+      *
+      * @return Return true if a normal can be computed, false otherwise.
+      */
     bool computePointNormal2d (boost::shared_ptr<std::vector<int>>& indices,
                                Eigen::Vector3f &line_parameters) const;
 
-    // Remet le normal dans le bon sens
-    void flipNormalTowardsViewpoint (const Point &point, Eigen::Matrix<double, 3, 1>& normal) const;
+
+    /** \brief Flip (in place) the estimated normal of a point towards a given viewpoint.
+     *
+     * @param point The normal location.
+     * @param normal The normal vector to transform.
+     */
+   void flipNormalTowardsViewpoint (const Point &point, Eigen::Matrix<double, 3, 1>& normal) const;
+
+    /** \brief Flip (in place) the estimated normal of a point towards a given viewpoint.
+     *
+     * @param point The normal location.
+     * @param x normal x component.
+     * @param y normal y component.
+     * @param z normal z component.
+     */
+   void flipNormalTowardsViewpoint (const Point &point, float& x, float& y, float& z) const;
 
 
-    void flipNormalTowardsViewpoint (const Point &point, float& x, float& y, float& z) const;
+
+   double m_search_radius; ///< Max distance between the estimate normal position with each neighbour for take them on normal estimation.
+   unsigned int m_k; ///< Max cluster size for estimate normal.
+
+   IndicesPtr m_indices;///< Indices from input cloud.
+   PtrkdTree m_kd_tree; ///< Search object to figure out neighbours.
+   ConstPtrCloud m_in_cloud; ///< Input cloud pointer to deal with.
 
 
-
-    double m_search_radius;
-    unsigned int m_k;
-
-    IndicesPtr m_indices;
-    PtrkdTree m_kd_tree;
-    ConstPtrCloud m_in_cloud;
-
-
-    Point m_view_point;
+   Point m_view_point; ///< View points used for normal fliping (default (0,0)).
 };
 
 
--- include/PCA2D.h
+++ include/PCA2D.h
@@ -21,54 +21,106 @@ typedef pcl::search::Search<Point>::Ptr PtrkdTree;
 typedef boost::shared_ptr<std::vector<int> > IndicesPtr;
 
 
+/***
+ *  \brief A self implemented PCA using only 2 dim. That ensure to always have a normal estimation on z=0 plan.
+ */
 class PCA2D {
 public:
 
+    /** \brief Empty constructor
+     */
     PCA2D() : m_computed(false), m_indices(new std::vector<int>), m_cloud(nullptr){}
 
-
+    /**
+     * \brief Perform the PCA computation.
+     */
     void initCompute();
 
-
+    /**
+     *  \brief Provide a input  cloud pointer to deal with.
+     *  @param cloud Cloud to deal with.
+     */
     void setInputCloud(const ConstPtrCloud& cloud);
 
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+     * \param indices indices a pointer to the indices that represent the input data.
+     */
     void setIndices(const pcl::PointIndices& indices);
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+     * \param indices indices a pointer to the indices that represent the input data.
+     */
     void setIndices(const pcl::PointIndicesPtr& indices);
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+     * \param indices indices a pointer to the indices that represent the input data.
+     */
     void setIndices(const pcl::PointIndicesConstPtr& indices);
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+     * \param indices indices a pointer to the indices that represent the input data.
+     */
     void setIndices(const boost::shared_ptr<std::vector<int>>& indices);
+    /**
+     * \brief Provide a pointer to the vector of indices that represents the input data.
+     * \param indices indices a pointer to the indices that represent the input data.
+     */
     void setIndices(const std::vector<int>& indices);
 
 
+    /** \brief Return the mean computed, if PCA isn't computed, it will perform it.
+     *
+     * @return The mean vector from PCA algorithm.
+     */
     Eigen::Vector2f getMean() {
         if(!m_computed) this->initCompute();
         return m_mean;
     }
 
+    /** \brief Return the eigen Vector in Eigen Matrix , if PCA isn't computed, it will perform it.
+     *
+     * @return The EigenVectors matrix from PCA algorithm.
+     */
     Eigen::Matrix2f getEigenVectors() {
         if(!m_computed) this->initCompute();
         return m_eigen_vectors;
     }
 
+    /** \brief Return the eigen Vector in Eigen Matrix , if PCA isn't computed, it will perform it.
+     *
+     * @return The EigenVectors matrix from PCA algorithm.
+     */
     Eigen::Vector2f getEigenValues() {
         if(!m_computed) this->initCompute();
         return m_eigen_values;
     }
 
+    /**
+     *  \brief Project point into PCA cartesian system.
+     * @param input Point to project.
+     * @param projection Point to fill with projection.
+     */
     void project(const Point & input, Point& projection);
 
+    /**
+     *  \brief Project input cloud into PCA cartesian system.
+     * @param input cloud with points to project.
+     * @param projection Cloud to fill with projected points.
+     */
     void project(const ConstPtrCloud& in_cloud, PtrCloud& out_cloud);
 
 
 private:
 
 
-    bool m_computed;
-    IndicesPtr m_indices;
-    ConstPtrCloud m_cloud;
+    bool m_computed; ///< True if PCA have been performed, false otherwise.
+    IndicesPtr m_indices; ///< Indice to use with input cloud.
+    ConstPtrCloud m_cloud;///< InputCloud to deal with.
 
-    Eigen::Vector2f m_mean;
-    Eigen::Matrix2f m_eigen_vectors;
-    Eigen::Vector2f m_eigen_values;
+    Eigen::Vector2f m_mean; ///< Centroid of input cloud
+    Eigen::Matrix2f m_eigen_vectors; ///< Eigen vector in matrix form.
+    Eigen::Vector2f m_eigen_values;///< Eigen scalar in vector.
 
 };
 
